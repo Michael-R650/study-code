b22828da560b3066b7aee9a55e8ae37f4e007e8e
Michael-R650 <reichle.michae-it25@it.dhbw-ravensburg.de>
2026-01-30 13:34:33 +0100

    aenderung

--- courses/coding-in-C/Aufgaben/L1/Hello_World.c
+++ courses/coding-in-C/Aufgaben/L1/Hello_World.c
@@ -2,5 +2,9 @@
 
 int main(){
     printf("Hello World");
+    char test[100];
+    //scanset
+    //max 99 zeichen einlesen mit leerzeichen etc ^\n, bis ein zeilenumbruch kommt und diesen dann mit %*c verwerfen
+    scanf("%99[^\n]%*c",test);
     return 0;
 }
