faa6c1a4d6661acc1e0c7cb3d22b66188fa18cc6
Michael-R650 <reichle.michae-it25@it.dhbw-ravensburg.de>
2026-01-31 13:14:42 +0100

    Aufgabe Matrix 1 erledigt

--- courses/coding-in-C/Aufgaben/Aufgabe_Array_2d/Aufgabe_Array_2d.c
+++ courses/coding-in-C/Aufgaben/Aufgabe_Array_2d/Aufgabe_Array_2d.c
@@ -1,25 +1,50 @@
 #include <stdio.h>
 
-int main(){
-    //Matrizen mit Werten
-    int matrix1[2][2]={{5,9},{3,6}};
-    int matrix2[2][2]={{0,3},{2,1}};
-    //Matrix fürs Zwischenspeichern
-    int speicher[2][2]={{},{}};
-    //Schleife zum vertauschen
+// Matrizen mit Werten
+int matrix1[2][2] = {{5, 9}, {3, 6}};
+int matrix2[2][2] = {{0, 3}, {2, 1}};
+// Matrix fürs Zwischenspeichern
+int speicher[2][2] = {{}, {}};
+
+//Methode zum Ausgeben der Werte in klarer Struktur
+void ausgabe()
+{
+    printf("|");
+    for (int i = 0; i <= 1; i++)
+    {
+
+        for (int j = 0; j <= 1; j++)
+        {
+            printf("%u", matrix1[i][j]);
+        }
+        printf("|");
+    }
+    printf("\n|");
+    for (int i = 0; i <= 1; i++)
+    {
+        for (int j = 0; j <= 1; j++)
+        {
+            printf("%u", matrix2[i][j]);
+        }
+        printf("|");
+    }
+    printf("\n");
+}
+
+int main()
+{
     printf("Vor dem Vertauschen:\n");
-    printf("%d\n",matrix1);
-    printf("%d\n",matrix2);
-    for(int i =0;i<=1;i++){
-        for(int j = 0; j<=1;j++){
-            speicher[i][j]= matrix1[i][j];
-            matrix1[i][j]=matrix2[i][j];
-            matrix2[i][j]=speicher[i][j];
-            
+    ausgabe();
+//Schleifen zum Vertauschen der Werte
+    for (int i = 0; i <= 1; i++)
+    {
+        for (int j = 0; j <= 1; j++)
+        {
+            speicher[i][j] = matrix1[i][j];
+            matrix1[i][j] = matrix2[i][j];
+            matrix2[i][j] = speicher[i][j];
         }
     }
-    printf("Nach dem Vertauschen\n");
-    printf("%d\n",matrix1);
-    printf("%d\n",matrix2);
-
+    printf("Nach dem Vertauschen:\n");
+    ausgabe();
 }
