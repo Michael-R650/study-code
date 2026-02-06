2f6e188def60c25dfc7c99d5effb45c5a224811e
Michael-R650 <reichle.michae-it25@it.dhbw-ravensburg.de>
2026-02-06 14:30:52 +0100

    Pointer Aufgabe angefangen

---
+++ courses/coding-in-C/Aufgaben/Pointer_Aufgabe/Pointeraufgabe.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int myNumbers[5] = {10, 20, 30, 40, 50};
+    int *p1 = (myNumbers + 1);
+    int *p2 = (myNumbers + 4);
+    printf("Pointer 1 adress: %p\n", &p1);
+    printf("Pointer 2 adress: %p\n", &p2);
+    printf("Pointer diffrence: %d\n", (p2 - p1));
+    /*  Pointer 1 adress: 00000000005FFE28
+        Pointer 2 adress: 00000000005FFE20
+        Pointer diffrence: 3
+        Die Pointer liegen 3 Adressen nebeneinander was 8 Byte sind
+    */
+}
