b2e5c7b24b41a47731d0d9e4cc572bc7a711b2ab
Michael-R650 <michaelvonreichle@gmail.comgit config --global user.name>
2026-04-14 20:33:19 +0200

    lap 2 accomblished

---
+++ courses/coding-in-C++/Lab_2/coding/section2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+class Note{
+private:
+std::string* text;
+public:
+Note(std::string par_text);
+Note(const Note &par_note);
+~Note();
+bool display();
+};
+/**
+ * @brief Construct a new Note:: Note object
+ * 
+ * @param par_text 
+ */
+Note::Note(std::string par_text){
+text = new std::string;
+*text = par_text;
+};
+Note::Note(const Note &par_note){
+text = new std::string;
+*text = *par_note.text;
+};
+/**
+ * @brief Destroy the Note:: Note object
+ * 
+ */
+Note::~Note(){
+    delete text;
+    text = nullptr;
+};
+/**
+ * @brief displays Note
+ * 
+ * @return true 
+ * @return false 
+ */
+bool Note::display(){
+    std::cout<<*text;
+    return true;
+};
+int main(){
+Note note1("Note 1");
+Note note2(note1);
+note2.display();
+note2.~Note();
+    return 0;
+}
+/**
+ * Bei destruktur wird 2 mal den gleichen Speicherort versucht zum löschen
+ * 
+ */
