ccc83f90233259be7fd9cf39af3114600027a096
Michael-R650 <michaelvonreichle@gmail.comgit config --global user.name>
2026-05-05 21:42:57 +0200

    push

--- courses/coding-in-C++/Lab_3/coding/section2/main.cpp
+++ courses/coding-in-C++/Lab_3/coding/section2/main.cpp
@@ -3,7 +3,7 @@
 #include "User.hpp"
 int main(){
 Student student1("Michael",2100,1);
-Instructor instructor1("Kevin", 1100,0);
+Instructor instructor1("Kevin", 1100,0); 
 student1.printRole();
 instructor1.printRole();
 
