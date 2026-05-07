f207fe4ea0397557a1b5065181861d55ac0bd887
Michael-R650 <michaelvonreichle@gmail.comgit config --global user.name>
2026-05-07 16:39:53 +0200

    created swap function

--- courses/coding-in-C++/Lab_5/coding/section 1/emergencyPayloadSwitch.hpp
+++ courses/coding-in-C++/Lab_5/coding/section 1/emergencyPayloadSwitch.hpp
@@ -1,6 +1,12 @@
 #ifndef PAYLOAD
 #define PAYLOAD
 template <typename T>
+/**
+ * @brief sawp a and b
+ *  swap of a and b mit typename T
+ * @param a 
+ * @param b 
+ */
 void swap(T &a, T &b)
 {
     T u = a;
--- courses/coding-in-C++/Lab_5/coding/section 1/main.cpp
+++ courses/coding-in-C++/Lab_5/coding/section 1/main.cpp
@@ -4,8 +4,20 @@ int main()
 {
     int i = 10;
     int j = 9;
+    std::string val1 = "Hi";
+    std::string val2 = "Tschau";
+    double i1 = 1.3;
+    double j1 = 5;
+
+    //Sawp funktionen
     std::cout << i << " " << j << "\n";
     swap(i, j);
     std::cout << i << " " << j << "\n";
+    std::cout << val1 << " " << val2 << "\n";
+    swap(val1, val2);
+    std::cout << val1 << " " << val2 << "\n";
+    std::cout << i1 << " " << j1 << "\n";
+    swap(i1, j1);
+    std::cout << i1 << " " << j1 << "\n";
     return 0;
 };
