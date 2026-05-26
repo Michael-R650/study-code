4b85ec056f632a1af57d28bb8a5d89ed4a58b126
Michael-R650 <michaelvonreichle@gmail.comgit config --global user.name>
2026-05-26 22:28:10 +0200

    corrected path of bugHunt

--- courses/coding-in-C++/Lab_6/solution/assistance_system.hpp
+++ courses/coding-in-C++/Lab_6/solution/assistance_system.hpp
@@ -62,7 +62,7 @@ public:
 class AdaptiveCruiseControl
 {
 private:
-    static constexpr double SPEED_STEP_KMH = 5.0; //MUSS DAS STATIC SEIN?
+    static constexpr double SPEED_STEP_KMH = 5.0;
 
     double target_speed_kmh;
     double minimum_distance_m;
